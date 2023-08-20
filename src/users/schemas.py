b6722bc58f66b80c4fdd7dfab1c67fa7bc267361
Rostyslav <rostyslavputnikov@gmail.com>
2023-08-20 21:53:18 +0300

    src/users/schemas.py updated

--- src/users/schemas.py
+++ src/users/schemas.py
@@ -1,1 +1,54 @@
 """src/users/schemas.py"""
+
+from datetime import datetime
+from typing import Optional
+from uuid import UUID
+
+from fastapi_users.schemas import BaseUser, BaseUserCreate, BaseUserUpdate
+from pydantic import ConfigDict, EmailStr, validator
+
+
+class UserRead(BaseUser[UUID]):
+    """Pydantic User Out schema"""
+
+    id: UUID
+    email: str
+    phone_number: str
+    first_name: Optional[str]
+    last_name: Optional[str]
+    created_date: datetime
+
+    __config__ = ConfigDict(from_attributes=True)
+
+
+class UserCreate(BaseUserCreate):
+    """Pydantic User In schema"""
+
+    email: EmailStr
+    phone_number: str
+    password: str
+    first_name: Optional[str] = None
+    last_name: Optional[str] = None
+
+    @validator("password")
+    def validate_password(cls, password):  # pylint: disable=E0213
+        if len(password) < 8:
+            raise ValueError("Password must be at least 8 characters long")
+        if not any(_.isupper() for _ in password):
+            raise ValueError(
+                "Password must contain at least one uppercase letter"
+            )
+        if not any(_.islower() for _ in password):
+            raise ValueError(
+                "Password must contain at least one lowercase letter"
+            )
+        if not any(_.isdigit() for _ in password):
+            raise ValueError("Password must contain at least one digit")
+        return password
+
+
+class UserUpdate(BaseUserUpdate):
+    """Pydantic User In schema"""
+
+    first_name: Optional[str] = None
+    last_name: Optional[str] = None
