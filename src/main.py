43a44535de2cb0b7100c9c85ac63f5476f669132
Rostyslav <rostyslavputnikov@gmail.com>
2023-08-20 18:27:17 +0300

    main.py updated

--- src/main.py
+++ src/main.py
@@ -1,1 +1,11 @@
 """src/main.py"""
+
+from fastapi import FastAPI
+
+app = FastAPI(title="Beauty Shop")
+
+
+@app.get("/", name="Welcome Page")
+def root():
+    """Welcome Page"""
+    return {"message": "Welcome"}
