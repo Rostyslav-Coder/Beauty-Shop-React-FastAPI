7e2536ab73121e600f83b418b9ebda6046afa702
Rostyslav <rostyslavputnikov@gmail.com>
2023-08-21 16:02:38 +0300

    src/employees/constants.py updated

--- src/employees/constants.py
+++ src/employees/constants.py
@@ -1,1 +1,18 @@
 """src/employees/constants.py"""
+
+from enum import Enum
+
+
+class Profession(Enum):
+    """Class of professions of the salon employees"""
+
+    HAIR_STYLIST = "Hair stylist"
+    MAKEUP_ARTIST = "Makeup artist"
+    ESTHETICIAN = "Esthetician"
+    NAIL_TECHNICIAN = "Nail technician"
+    EYELASH_SPECIALIST = "Eyelash specialist"
+    MASSAGE_THERAPIST = "Massage therapist"
+    TATTOO_ARTIST = "Tattoo artist"
+    TANNING_CONSULTANT = "Tanning consultant"
+    BEAUTY_CONSULTANT = "Beauty consultant"
+    SALON_MANAGER = "Salon manager"
