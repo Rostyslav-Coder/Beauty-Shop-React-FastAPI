e7ed351e8ec0c786a9377f2ede966bbabee5e132
Rostyslav <rostyslavputnikov@gmail.com>
2023-08-21 22:34:38 +0300

    src/employees/schemas.py updated

--- src/employees/schemas.py
+++ src/employees/schemas.py
@@ -1,1 +1,24 @@
 """src/employees/schemas.py"""
+
+from uuid import UUID
+
+from pydantic import BaseModel, ConfigDict
+
+from src.employees.constants import EmploymentContract, Profession
+
+
+class EmployeeRead(BaseModel):
+    """Pydantic Employee Out schema"""
+
+    profession: Profession
+    photo_url: str
+    images_urls: list[str]
+    employment_contract: EmploymentContract
+
+    __config__ = ConfigDict(from_attributes=True)
+
+
+class EmployeeCreate(EmployeeRead):
+    """Pydantic Employee In schema"""
+
+    user_id: UUID
