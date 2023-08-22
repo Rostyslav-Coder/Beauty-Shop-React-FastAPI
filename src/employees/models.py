d23dd493616a5b0cf4bcc4c008d2b4d5ca13a12c
Rostyslav <rostyslavputnikov@gmail.com>
2023-08-22 15:59:25 +0300

    src/employees/models.py updated

--- src/employees/models.py
+++ src/employees/models.py
@@ -1,10 +1,20 @@
 """src/employees/models.py"""
 
+from datetime import timedelta
+from decimal import Decimal
 from uuid import uuid4
 
-from sqlalchemy import ARRAY, Enum, ForeignKey, LargeBinary, String
+from sqlalchemy import (
+    ARRAY,
+    Enum,
+    ForeignKey,
+    Interval,
+    LargeBinary,
+    Numeric,
+    String,
+)
 from sqlalchemy.dialects.postgresql import UUID
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from src.database import Base
 from src.employees.constants import EmploymentContract, Profession
@@ -34,3 +44,28 @@ class Employee(Base):
     user_id: Mapped[UUID] = mapped_column(
         UUID(as_uuid=True), ForeignKey("users.id")
     )
+    services = relationship("Service", back_populates="employee")
+
+
+class Service(Base):
+    """Class for creating a Service table in the Database"""
+
+    __tablename__ = "services"
+
+    service_id: Mapped[UUID] = mapped_column(
+        UUID(as_uuid=True), primary_key=True, default=uuid4
+    )
+    service_name: Mapped[str] = mapped_column(
+        String(length=50), nullable=False
+    )
+    service_duration: Mapped[timedelta] = mapped_column(
+        Interval, nullable=False
+    )
+    service_cost: Mapped[Decimal] = mapped_column(
+        Numeric(10, 2), nullable=False
+    )
+    employee_id: Mapped[UUID] = mapped_column(
+        UUID(as_uuid=True), ForeignKey("employees.employee_id")
+    )
+
+    employee = relationship("Employee", back_populates="services")
