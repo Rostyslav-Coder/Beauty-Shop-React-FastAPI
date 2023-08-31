cddcfde262e8e7149871f36f36b82767c3b5390b
Rostyslav <rostyslavputnikov@gmail.com>
2023-08-31 11:56:33 +0300

    src/infrastructure/detabase package added in project

---
+++ src/infrastructure/database/tables.py
@@ -0,0 +1,29 @@
+"""src/infrastructure/database/tables.py"""
+
+from typing import TypeVar
+
+from sqlalchemy import Column, Integer, MetaData
+from sqlalchemy.orm import declarative_base
+
+__all__ = ()
+
+meta = MetaData(
+    naming_convention={
+        "ix": "ix_%(column_0_label)s",
+        "uq": "uq_%(table_name)s_%(column_0_name)s",
+        "ck": "ck_%(table_name)s_`%(constraint_name)s`",
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+        "pk": "pk_%(table_name)s",
+    }
+)
+
+
+class _Base:
+    """Base class for all database models."""
+
+    id = Column(Integer, primary_key=True)
+
+
+Base = declarative_base(cls=_Base, metadata=meta)
+
+ConcreteTable = TypeVar("ConcreteTable", bound=Base)  # type: ignore
