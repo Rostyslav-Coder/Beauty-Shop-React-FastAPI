d3ac1840d2e1d57df297bb0e0c70ce740363a789
Rostyslav <rostyslavputnikov@gmail.com>
2023-09-01 14:53:48 +0300

    src/domain/booking package added in project

---
+++ src/domain/bookings/__init__.py
@@ -0,0 +1,4 @@
+"""src/domain/bookings/__init__.py"""
+
+from src.domain.bookings.models import *  # noqa: F401, F403
+from src.domain.bookings.repository import *  # noqa: F401, F403
---
+++ src/domain/bookings/repository.py
@@ -0,0 +1,32 @@
+"""src/domain/bookings/repository.py"""
+
+from typing import Any, AsyncGenerator
+
+from src.domain.bookings import Booking, BookingUncommited
+from src.infrastructure.database import BaseRepository, BookingTable
+
+__all__ = ("BookingRepository",)
+
+
+class BookingRepository(BaseRepository[BookingTable]):
+    schema_class = BookingTable
+
+    async def all(self) -> AsyncGenerator[Booking, None]:
+        async for instance in self._all():
+            yield Booking.from_orm(instance)
+
+    async def get(self, id_: int) -> Booking:
+        instance = await self._get(key="id", value=id_)
+        return Booking.from_orm(instance)
+
+    async def create(self, schema: BookingUncommited) -> Booking:
+        instance: BookingTable = await self._save(schema.dict())
+        return Booking.from_orm(instance)
+
+    async def update(
+        self, key_: str, value_: Any, payload_: BookingUncommited
+    ) -> Booking:
+        instance: BookingTable = await self._update(
+            key=key_, value=value_, payload=payload_.dict()
+        )
+        return Booking.from_orm(instance)
