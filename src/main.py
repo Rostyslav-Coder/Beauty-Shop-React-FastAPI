17d2e217f90240fb54fefa6b154165ed2df26bad
Rostyslav <rostyslavputnikov@gmail.com>
2023-09-01 16:06:37 +0300

    src/main.py updated

--- src/main.py
+++ src/main.py
@@ -1,7 +1,40 @@
 """src/main.py"""
 
+import uvicorn
 from fastapi import FastAPI
+from loguru import logger
+
+from src.config import settings
+from src.infrastructure import application, database
+from src.presentation import rest
+
+# Adjust the logging
+# -------------------------------
+logger.add(
+    "".join(
+        [
+            str(settings.root_dir),
+            "/logs/",
+            settings.logging.file.lower(),
+            ".log",
+        ]
+    ),
+    format=settings.logging.format,
+    rotation=settings.logging.rotation,
+    compression=settings.logging.compression,
+    level="INFO",
+)
+
 
 # Adjust the application
 # -------------------------------
-app = FastAPI()
+app: FastAPI = application.create(
+    debug=settings.debug,
+    rest_routers=(rest.users.router,),
+    startup_tasks=[database.create_tables],
+    shutdown_tasks=[],
+)
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000)
