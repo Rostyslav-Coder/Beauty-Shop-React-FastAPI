10838c1ef07d399aec23a6722c9a2cdbbd0e39bb
Rostyslav <rostyslavputnikov@gmail.com>
2023-09-01 23:16:08 +0300

    src/application/authentication package updated

--- src/application/authentication/dependency_injection.py
+++ src/application/authentication/dependency_injection.py
@@ -9,10 +9,14 @@ from pydantic import ValidationError
 
 from src.config import settings
 from src.domain.authentication import TokenPayload
+from src.domain.constants import UserRole
 from src.domain.users import User, UsersRepository
-from src.infrastructure.errors import AuthenticationError
+from src.infrastructure.errors import AuthenticationError, AuthorizationError
 
-__all__ = ("get_current_user",)
+__all__ = (
+    "get_current_user",
+    "RoleRequired",
+)
 
 oauth2_oauth = OAuth2PasswordBearer(
     tokenUrl="/auth/openapi",
@@ -34,7 +38,7 @@ async def get_current_user(token: str = Depends(oauth2_oauth)) -> User:
     except (JWTError, ValidationError):
         raise AuthenticationError
 
-    user = await UsersRepository().get(id_=token_payload.sub)
+    user = await UsersRepository().get(key_="id", value_=token_payload.sub)
 
     # TODO: Check if the token is in the blacklist
 
@@ -42,3 +46,16 @@ async def get_current_user(token: str = Depends(oauth2_oauth)) -> User:
 
 
 # TODO: Create token blacklist & Logout f-tion
+
+
+class RoleRequired:
+    """Require users role"""
+
+    def __init__(self, role: UserRole):
+        self.role = role
+
+    async def __call__(self, user: User = Depends(get_current_user)):
+        if user.role != self.role:
+            raise AuthorizationError
+
+        return user
