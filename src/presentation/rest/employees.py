f3dbdec3c25953af617945ddfc6dd802768a46aa
Rostyslav <rostyslavputnikov@gmail.com>
2023-09-03 19:56:45 +0300

    src/presentation/rest package updated

--- src/presentation/rest/employees.py
+++ src/presentation/rest/employees.py
@@ -3,7 +3,7 @@
 from fastapi import APIRouter, Depends, Request, status
 
 from src.application.authentication import RoleRequired
-from src.domain.constants import UserRole
+from src.domain.constants import UserRole, WorkingDays
 from src.domain.employees import (
     Employee,
     EmployeeCreateRequestBody,
@@ -33,6 +33,41 @@ async def employee_create(
     # Save new employee to the database
     employee: Employee = await EmployeeRepository().create(
         EmployeeUncommited(**schema.dict())
+    )
+    employee_public = EmployeePublic.from_orm(employee)
+
+    return Response[EmployeePublic](result=employee_public)
+
+
+@router.put("/update_days", status_code=status.HTTP_202_ACCEPTED)
+@transaction
+async def employee_update_days(
+    _: Request,
+    new_days: WorkingDays,
+    user: User = Depends(RoleRequired(UserRole.EMPLOYEE)),
+) -> Response[EmployeePublic]:
+    """Update employees working days"""
+
+    # Update employees working days
+    payload = {"working_days": new_days}
+    employee: Employee = await EmployeeRepository().update(
+        key_="user_id", value_=user.id, payload_=payload
+    )
+    employee_public = EmployeePublic.from_orm(employee)
+
+    return Response[EmployeePublic](result=employee_public)
+
+
+@router.get("/get", status_code=status.HTTP_200_OK)
+@transaction
+async def employee_get(
+    _: Request,
+    employee_id: int,
+) -> Response[EmployeePublic]:
+    """Get employee with user, full model"""
+
+    employee: Employee = await EmployeeRepository().get(
+        key_="id", value_=employee_id
     )
     employee_public = EmployeePublic.from_orm(employee)
 
