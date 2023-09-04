047044590cb5aae13c90025c5412b1af16287773
Rostyslav <rostyslavputnikov@gmail.com>
2023-09-04 22:21:55 +0300

    src/presentation/rest package updated

--- src/presentation/rest/__init__.py
+++ src/presentation/rest/__init__.py
@@ -2,4 +2,5 @@
 
 from src.presentation.rest.authenticate import *  # noqa: F401, F403
 from src.presentation.rest.employees import *  # noqa: F401, F403
+from src.presentation.rest.services import *  # noqa: F401, F403
 from src.presentation.rest.users import *  # noqa: F401, F403
--- src/presentation/rest/services.py
+++ src/presentation/rest/services.py
@@ -1,15 +1,18 @@
 """src/presentation/rest/services.py"""
 
+from datetime import timedelta
+from decimal import Decimal
+
 from fastapi import APIRouter, Depends, Request, status
 
 from src.application.authentication import RoleRequired  # , get_current_user
 from src.domain.constants import UserRole
 from src.domain.employees import Employee, EmployeeRepository
 from src.domain.services import (
-    SericeRepository,
     Service,
     ServiceCreateRequestBody,
     ServicePublic,
+    ServiceRepository,
     ServiceUncommited,
 )
 from src.domain.users import User
@@ -37,9 +40,43 @@ async def services_add(
     schema.employee_id = employee.id
 
     # Save new service to the database
-    service: Service = SericeRepository().create(
+    service: Service = await ServiceRepository().create(
         ServiceUncommited(**schema.dict())
     )
-    service_public = ServicePublic().from_orm(service)
+    service_public = ServicePublic.from_orm(service)
+
+    return Response[ServicePublic](result=service_public)
+
+
+@router.put("/update", status_code=status.HTTP_202_ACCEPTED)
+@transaction
+async def services_update(
+    _: Request,
+    new_title: str,
+    new_duration: timedelta,
+    new_price: Decimal,
+    user: User = Depends(RoleRequired(UserRole.EMPLOYEE)),
+) -> Response[ServicePublic]:
+    """Update values in a service data"""
+
+    # Get the employee associated with the user
+    employee: Employee = await EmployeeRepository().get(
+        key_="user_id", value_=user.id
+    )
+
+    # Preparing Data for Update
+    payload = {}
+    if new_title:
+        payload["title"] = new_title
+    if new_duration:
+        payload["duration"] = new_duration
+    if new_price:
+        payload["price"] = new_price
+
+    # Update service from database
+    service: Service = ServiceRepository().update(
+        key_="employee_id", value_=employee.id, payload_=payload
+    )
+    service_public = ServicePublic.from_orm(service)
 
     return Response[ServicePublic](result=service_public)
