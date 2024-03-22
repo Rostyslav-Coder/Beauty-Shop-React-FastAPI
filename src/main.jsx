bc756ac164242b1c31d25a0e39a88a97c617ab32
Rostyslav-Coder <rostyslavputnikov@gmail.com>
2024-03-23 00:16:45 +0200

    React.js added in project

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+// ============ MAIN COMPONENT MODULE  ============ //
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './components/App.jsx';
+import './main.css';
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+)
