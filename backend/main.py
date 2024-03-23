ccb1b4b96e65e71b5c13d30647cbf03c237230b1
Rostyslav-Coder <rostyslavputnikov@gmail.com>
2024-03-23 11:15:47 +0200

    project updated

--- backend/main.py
+++ backend/main.py
@@ -2,11 +2,13 @@
 
 import uvicorn
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from loguru import logger
 
 from backend.config import settings
 from backend.infrastructure import application, database
 from backend.presentation import rest
+
 
 # Adjust the logging
 # -------------------------------
@@ -39,6 +41,10 @@ app: FastAPI = application.create(
     startup_tasks=[database.create_tables],
     shutdown_tasks=[],
 )
+
+
+# # Mount static files
+# app.mount("/", StaticFiles(directory="dist"), name="frontend")
 
 
 if __name__ == "__main__":
