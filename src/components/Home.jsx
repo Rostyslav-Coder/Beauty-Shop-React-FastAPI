73b0f99f599eec423e02763425beb144af5ee4c4
Rostyslav-Coder <rostyslavputnikov@gmail.com>
2024-04-24 20:25:52 +0300

    frontend updated

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,9 @@
 // ============ HOME PAGE COMPONENT MODULE  ============ //
 
 import { useState, useEffect } from 'react';
-import { ParallaxBanner, ParallaxBannerLayer, Parallax } from 'react-scroll-parallax';
+import { ParallaxBanner, Parallax } from 'react-scroll-parallax';
 
-import homeBG from '../assets/home/home-bg.jpg';
+// import homeBG from '../assets/home/home-bg.jpg';
 import goddess from '../assets/home/goddess.webp'
 import threeGoddesses from '../assets/home/three-goddesses.webp'
 import '../styles/Home.css';
@@ -40,11 +40,6 @@ const Home = () => {
 
 	return (
 		<ParallaxBanner className='home'>
-			<ParallaxBannerLayer
-				className='home__bg'
-				image={homeBG}
-				speed={-25}
-			/>
 			<div className='home__img-wwrapper'>
 				<img className='home__img' src={imgSrc} alt='goddesses' style={goddessStyle} />
 			</div>
@@ -58,32 +53,72 @@ const Home = () => {
 					Discover a place where harmony fills every corner, and each service is a masterpiece.
 				</h2>
 			</div>
-			<Parallax speed={10}>
-				<p className='home__text'>
+			<Parallax
+				speed={30}
+				translateX={[30, -30]}
+				className='home__text'
+			>
+				<p>
 					<strong>An Exclusive Experience </strong>
-					&mdash; Our salon offers more than just beauty treatments; we provide a holistic approach to self-care. We create an ambiance where every client feels special and valued.
+					&mdash; Our salon offers more than just beauty treatments, we provide a holistic approach to self-care. We create an ambiance where every client feels special and valued.
 				</p>
-				<p className='home__text'>
+			</Parallax>
+			<Parallax
+				speed={30}
+				translateX={[30, -30]}
+				className='home__text'
+			>
+				<p>
 					<strong>Personalized Care </strong>
 					&mdash; Your satisfaction is our top priority. We listen attentively to your wishes and aim to exceed expectations, offering services that are perfectly suited to you.
 				</p>
-				<p className='home__text'>
+			</Parallax>
+			<Parallax
+				speed={30}
+				translateX={[30, -30]}
+				className='home__text'
+			>
+				<p>
 					<strong>Health and Eco-Friendliness </strong>
 					&mdash; The health of our clients and the planet is at the heart of everything we do. By using top-quality products, we ensure the safety and effectiveness of our procedures.
 				</p>
-				<p className='home__text'>
+			</Parallax>
+			<Parallax
+				speed={30}
+				translateX={[30, -30]}
+				className='home__text'
+			>
+				<p>
 					<strong>Expertise and Innovation </strong>
 					&mdash; Our talented professionals continuously refine their skills and stay abreast of the latest beauty trends to offer you the most modern and effective solutions.
 				</p>
-				<p className='home__text'>
+			</Parallax>
+			<Parallax
+				speed={30}
+				translateX={[30, -30]}
+				className='home__text'
+			>
+				<p>
 					<strong>Events and Promotions </strong>
 					&mdash; We regularly host events and promotions so that every client can indulge in extra pleasures.
 				</p>
-				<p className='home__text'>
+			</Parallax>
+			<Parallax
+				speed={30}
+				translateX={[30, -30]}
+				className='home__text'
+			>
+				<p>
 					<strong>Community and Connection </strong>
-					&mdash; <span>BEAUTY SHOP</span> is not just about services; it&apos;s a community where everyone can find new friends, share their stories, and receive support.
+					&mdash; <span>BEAUTY SHOP</span> is not just about services, it&apos;s a community where everyone can find new friends, share their stories, and receive support.
 				</p>
-				<p className='home__text'>
+			</Parallax>
+			<Parallax
+				speed={30}
+				translateX={[30, -30]}
+				className='home__text'
+			>
+				<p>
 					Come and let us take care of you, for every day is an opportunity to become even more beautiful. <strong>BEAUTY SHOP</strong> awaits you to create beauty stories together that will inspire you and those around you.
 				</p>
 			</Parallax>
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,10 +1,69 @@
 // ============ SERVICES PAGE COMPONENT MODULE  ============ //
 
+import { useState, useEffect } from 'react';
+import { ParallaxBanner, Parallax } from 'react-scroll-parallax';
+
+import simpleRequest from '../request/simpleRequest';
 import '../styles/Services.css';
 
 
 const Services = () => {
-	return <div className='services'>This is Services page</div>
+	const [professions, setProfessions] = useState([]);
+	const [services, setServices] = useState([]);
+
+	useEffect(() => {
+		const PROFESSION_URL = '/professions/all';
+		const SERVICES_URL = '/services/all_by_profesion';
+
+		const fetchData = async () => {
+			const professionResult = await simpleRequest(PROFESSION_URL);
+			if (professionResult.error) {
+				setProfessions([]);
+			} else {
+				setProfessions(professionResult.result);
+				professionResult.result.forEach(async (profession) => {
+					const serviceResult = await simpleRequest(SERVICES_URL, { profession_id: profession.id });
+					if (serviceResult.error) {
+						setServices(prevServices => ({ ...prevServices, [profession.id]: [] }));
+					} else {
+						setServices(prevService => ({ ...prevService, [profession.id]: serviceResult.result }));
+					}
+				})
+			}
+		}
+
+		fetchData();
+	}, []);
+
+	return (
+		<ParallaxBanner className='services'>
+			<div className='services__title-block'>
+				<h1 className='services__title'>
+					Create Your Perfect Look.
+				</h1>
+				<h2 className='services__subTitle'>
+					From manicures to massages, we&apos;ll help you look unparalleled.
+				</h2>
+			</div>
+			{professions.map(profession => (
+				<Parallax
+					key={profession.id}
+					className='services__card'
+					speed={30}
+					translateX={[30, -30]}
+				>
+					<h2>{profession.name} SERVICES:</h2>
+					{services[profession.id]?.map(service => (
+						<div key={service.id} className='services__service'>
+							<h3>{service.name}</h3>
+							<p>{service.description}</p>
+							<p>Minimal price: $ {service.minPrice}</p>
+						</div>
+					))}
+				</Parallax>
+			))}
+		</ParallaxBanner>
+	);
 };
 
 export default Services;
